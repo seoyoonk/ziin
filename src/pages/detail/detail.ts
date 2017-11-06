1b580ebac3c47886f5f1d8d3f83d2a62d7d1f1ea
mimela98 <mimela98@gmail.com>
2017-11-06 15:09:42 +0900

    a

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -9,55 +9,44 @@ import { OrderPage } from '../order/order';
   templateUrl: 'detail.html'
 })
 export class DetailPage {
- 
 
-  data: any={rcmd_mem_nm:'test',goods_opt_list:[{sellprice:0}], detail_img_list:[]};
-  
+
+  data: any = { rcmd_mem_nm: 'test', goods_opt_list: [{ sellprice: 0 }], detail_img_list: [] };
+
   ionViewDidLoad() {
-    this.rest.selectOneGoodsRcmd({goods_no: this.params.data.goods_no} ).subscribe(
+    this.rest.selectOneGoodsRcmd({ goods_no: this.params.data.goods_no }).subscribe(
       res => {
-       
-        this.data = res.res_data;
-        if(res.res_data.detail_img !='')
-        {
-          this.data.detail_img_list = this.data.detail_img.split('|').filter(
-            (val) =>
-            {
-              if(val=="") return false;
-              else return true;
-            }) ;
 
-        } 
-      
+        this.data = res.res_data;
+        if (res.res_data.detail_img != '') {
+          this.data.detail_img_list = this.data.detail_img.split('|').filter(
+            (val) => {
+              if (val == "") return false;
+              else return true;
+            });
+
+        }
+
       },
       err => {
         alert("ERROR!: " + err);
       })
-      
+
   }
-  dismiss()
-  {
-      this.viewCtrl.dismiss();
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
-  goComment()
-  {
-    let modal = this.modalCtrl.create(CommentPage, {goods_no : this.params.data.goods_no});
+  goComment() {
+    let modal = this.modalCtrl.create(CommentPage, { goods_no: this.params.data.goods_no });
     modal.present();
 
   }
   constructor(public viewCtrl: ViewController, private modalCtrl: ModalController, public rest: RestProvider, public params: NavParams) {
-    
+
   }
-  goOrder(){
-    let modal = this.modalCtrl.create(OrderPage, 
-      {
-        goods_no : this.params.data.goods_no,
-        opt_no : this.data.goods_opt_list[0].opt_no,
-        sellprice : this.data.goods_opt_list[0].sellprice,
-        thumbnail : this.data.list_img_1,
-        max_order_cnt : this.data.tot_stock_cnt        
-      });
-      modal.present();
+  goOrder() {
+    let modal = this.modalCtrl.create(OrderPage, this.data);
+    modal.present();
   }
 
 }
